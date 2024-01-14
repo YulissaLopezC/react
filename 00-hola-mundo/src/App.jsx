4963c33da4623f3a3a580d3815b827605907903a
Yulissa <lyulissa5@gmail.com>
2024-01-13 21:48:15 -0500

    changing the prop

--- 00-hola-mundo/src/App.jsx
+++ 00-hola-mundo/src/App.jsx
@@ -4,9 +4,9 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 export function App () {
     return (
         <section className='App'>
-        <TwitterFollowCard isFollowing userName="YulissaLopezC" name="Yulissa Lopez Cortes"  />
-        <TwitterFollowCard isFollowing={false} userName="YulissaLopezC" name="Yulissa Lopez Cortes"  />
-        <TwitterFollowCard isFollowing userName="YulissaLopezC" name="Yulissa Lopez Cortes"  />
+        <TwitterFollowCard isFollowing userName="YulissaLopezC" name="Yulissa Lopez C"  />
+        <TwitterFollowCard isFollowing={false} userName="YulissaLopezC" name="Yulissa Lopez C"  />
+        <TwitterFollowCard isFollowing userName="YulissaLopezC" name="Yulissa Lopez C"  />
         </section>
     )
 }
